12f646b0b3c5abe01a7a88663af19d22470a8a4b
gaofanghuang <gaofanghuang@vip.qq.com>
2020-03-20 20:44:54 +0800

    1

--- pc/static/js/set.b61cefc5.js
+++
@@ -1,1 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["set"],{eed2:function(e,n,t){"use strict";t.r(n);var l=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v(" 设置 ")])},s=[],u=t("2877"),c={},r=Object(u["a"])(c,l,s,!1,null,null,null);n["default"]=r.exports}}]);
--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'? '/pc/': '/',
-  outputDir: '../pc/',
+  publicPath: '/',
+  outputDir: '../',
   assetsDir: 'static',
   lintOnSave: true,
   productionSourceMap: false,
